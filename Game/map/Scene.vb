557db6bf72f1ca4421fb00e8b615c09a199e9ba3
Jack Jiang <jackywathy24@gmail.com>
2017-08-12 01:57:00 +1000

    more ma-ps

--- Game/map/Scene.vb
+++ Game/map/Scene.vb
@@ -26,7 +26,12 @@ Public Class Scene
             AllItems.Add(item)
         Next
     End Sub
-
+    Sub SetBackground(name As String)
+        Me.Background = New BackgroundRender(TotalGridWidth, TotalGridHeight, My.Resources.ResourceManager.GetObject(name))
+    End Sub
+    Sub SetBackground(image as Image)
+        Me.Background = New BackgroundRender(TotalGridWidth, TotalGridHeight, image)
+    End Sub
 
     Public Background As BackgroundRender
     Public Player1 = Entities.player1
@@ -51,6 +56,7 @@ Public Class Scene
     End Sub
 
     Sub RenderScene(g As Graphics)
+        
         Background.Render(g)
         ' check collision before rending object
         Dim objects = GetObjInScene()
@@ -66,24 +72,70 @@ Public Class Scene
         Entities.player1.Render(g)
     End Sub
 
-    Public Sub ReadMapFromResource(name As String)
-        Dim byteArray = CType(My.Resources.ResourceManager.GetObject(name), Byte())
+
+
+    Public Shared Sub ReadMapFromResource(jsonName As String)
+        Dim byteArray = CType(My.Resources.ResourceManager.GetObject(jsonName), Byte())
         If byteArray(0) = 239 And byteArray(1) = 187 And byteArray(2) = 191 Then
             byteArray = byteArray.Skip(3).Take(byteArray.Length - 2).ToArray()
         End If
-        Dim str = System.Text.Encoding.UTF8.GetString(byteArray)
+        Dim str = Text.Encoding.UTF8.GetString(byteArray)
 
+        
+        Dim mapObject = JsonConvert.DeserializeObject(Of MapObject)(str)
+        
+        Dim outScene As New Scene
 
-        Console.Out.Write(str)
-        Dim MapObject As MapObject = JsonConvert.DeserializeObject(Of MapObject)(str)
+        ' add the background
+        outScene.SetBackground(mapObject.background)
 
+        ' add all blocks
+        For each pair as KeyValuePair(Of String, Ilist(OF Integer())) in mapObject.blocks
+            Dim name = pair.Key
+            For each params As Integer() In pair.Value
+                outScene.Add(outScene.RenderItemFactory(name, params))
+            Next
+        Next
+        Print("HI!")
+        
 
     End Sub
+    
+    Private Sub AssertLength(type As String, expected As Integer, given As Integer, array As Integer())
+        if expected <> given
+            Throw New InvalidJsonException(String.Format("Error in JSON, type={0}, given {1} elements when {2} expected - [ {3} ] ", 
+                                                         type, given, expected, String.Join(", ", array)))
+        End If
+    End Sub
+
+    Private Function RenderItemFactory(name As String, params As Integer()) As RenderObject
+        Dim out as RenderObject
+        Select Case name
+            Case "bbrick"
+                AssertLength("bbrick", 2, params.Length, params)
+                out = New BreakableBrick(New Point(params(0), params(1)))
+            Case "brickplatform"
+
+            Case Else
+                Throw New Exception(String.Format("No object with name {0}", name))
+        End Select
+        Return out
+        
+    End Function
+    Private Class InvalidJsonException 
+        Inherits Exception
+        Sub New(message As String)
+            MyBase.New(message)
+        End Sub
+
+    End Class
 End Class
 
+<JsonObject(ItemRequired:= Required.Always)>
 Public Class MapObject
     Public Property map_name As String
     Public Property blocks As Dictionary(Of String, IList(Of Integer()))
+    Public Property background As String
 
 
 
