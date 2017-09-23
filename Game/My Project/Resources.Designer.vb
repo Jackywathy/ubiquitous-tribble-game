eec534fcd85b2063ba1c0a02bbe0b1b1f273d5b5
Jack Jiang <jackywathy24@gmail.com>
2017-09-23 16:46:18 +1000

    stuffs

--- Game/My Project/Resources.Designer.vb
+++ Game/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property blockBelowGround() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("blockBelowGround", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property blockBrick() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("blockBrick", resourceCulture)
@@ -86,16 +96,6 @@ Namespace My.Resources
         Friend ReadOnly Property blockGround() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("blockGround", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property blockGround1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("blockGround1", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
---
+++ Game/renderObj/BelowGroundPlatform.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class BelowGroundPlatform
+    Inherits HitboxItem
+
+
+    ''' <summary>
+    ''' TODO make theme work
+    ''' </summary>
+    ''' <param name="width"></param>
+    ''' <param name="height"></param>
+    ''' <param name="location"></param>
+    ''' <param name="theme"></param>
+    ''' <param name="mapScene"></param>
+    Public Sub New(width As Integer, height As Integer, location As Point, theme As RenderTheme, mapScene As MapScene)
+        MyBase.New(width, height, location, GetFloor(width, height, theme), mapScene)
+    End Sub
+        
+
+    Private Shared Function GetFloor(width As Integer, height as Integer, theme As RenderTheme) As Image
+        Dim image = New Bitmap(width, height)
+        Using brush=New TextureBrush(My.Resources.blockBelowGround, WrapMode.Tile)
+            brush.TranslateTransform(0, 32)
+            Using g=Graphics.FromImage(image)
+                g.FillRectangle(brush, 0,0, image.Width, image.Height)
+            End Using
+        End Using
+        Return image
+    End Function
+
+    ''' <summary>
+    ''' 0 : x
+    ''' 1 : y
+    ''' 2 : width
+    ''' 3 : height
+    ''' </summary>
+    ''' <param name="params"></param>
+    Public Sub New (params As Object(), theme As RenderTheme, mapScene As MapScene)
+        Me.New(params(2)*32, params(3)*32, New Point(params(0)*32, params(1)*32), theme, mapScene)
+    End Sub
+
+    Public Overrides Sub Render(g As Graphics)
+        MyBase.Render(g)
+    End Sub
+End Class
