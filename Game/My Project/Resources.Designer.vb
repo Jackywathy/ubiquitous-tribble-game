ae81fbff4a6bca132bc161573df2ef77e55b21be
Vincent Liu <pkchu99@gmail.com>
2017-09-23 17:16:59 +1000

    add StaticDecoration class

--- Game/My Project/Resources.Designer.vb
+++ Game/My Project/Resources.Designer.vb
@@ -173,6 +173,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property castle() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("castle", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property cloud_big() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("cloud_big", resourceCulture)
---
+++ Game/renderObj/static/StaticDecoration.vb
@@ -0,0 +1,19 @@
+﻿Public Class StaticDecoration
+
+    Inherits StaticImage
+
+    ' images
+    Shared ReadOnly Property CloudSmall As Image = My.Resources.cloud_small
+    Shared ReadOnly Property CloudBig As Image = My.Resources.cloud_big
+    Shared ReadOnly Property HillSmall As Image = My.Resources.hill_small
+    Shared ReadOnly Property HillBig As Image = My.Resources.hill_big
+    Shared ReadOnly Property Castle As Image = My.Resources.castle
+
+    Shared ReadOnly Property Mushroom As Image = My.Resources.mushroom
+    Shared ReadOnly Property FireFlower As Image = My.Resources.f_flower_1
+
+    Public Sub New(location As Point, image As Image)
+        MyBase.New(image.Width, image.Height, location, image)
+    End Sub
+
+End Class
