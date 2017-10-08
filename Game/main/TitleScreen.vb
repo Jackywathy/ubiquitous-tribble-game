c7bc4ea6904fc2d05047c06623fc87b73f6c9da2
Vincent Liu <pkchu99@gmail.com>
2017-10-08 22:41:04 +1100

    added friendly exit

--- Game/main/TitleScreen.vb
+++ Game/main/TitleScreen.vb
@@ -3,4 +3,10 @@
         Dim helpTxt = "Super Mario Bros. HD 720p™ is a faithful recreation of the classic 1985 NES game. Use WASD to move, Space to shoot fireballs, and Esc to pause the game."
         MsgBox(helpTxt, MsgBoxStyle.Information, "Help")
     End Sub
+
+    Private Sub TitleScreen_WillClose(sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If MessageBox.Show("Are you sure you want to quit?", "Confirm exit", MessageBoxButtons.YesNo) <> DialogResult.Yes Then
+            e.Cancel = True
+        End If
+    End Sub
 End Class
